5803873ab785fb19fad3ae2f4eaf4281536ab71f
Rick South <rick@natamus.com>
2024-06-14 05:45:18 +0200

    Altered Damage source code for 1.21.0.

---
+++ NeoForge/src/main/java/com/natamus/altereddamage/neoforge/events/NeoForgeAlterDamageEvent.java
@@ -0,0 +1,22 @@
+package com.natamus.altereddamage.neoforge.events;
+
+import com.natamus.altereddamage.events.AlterDamageEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.neoforge.event.entity.living.LivingHurtEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeAlterDamageEvent {
+	@SubscribeEvent
+	public static void onEntityDamageTaken(LivingHurtEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+
+		float originalDamage = e.getAmount();
+		float newDamage = AlterDamageEvent.onEntityDamageTaken(livingEntity.level(), livingEntity, e.getSource(), originalDamage);
+
+		if (originalDamage != newDamage) {
+			e.setAmount(newDamage);
+		}
+	}
+}
