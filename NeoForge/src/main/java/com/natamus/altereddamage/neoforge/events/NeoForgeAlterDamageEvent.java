55deb5c46c3ba1a03bfada35d0da0e013abb7a11
Rick South <rick@natamus.com>
2024-06-26 00:03:04 +0200

    (v3.4) (1.21.0 Neo) Updated mod to work with NeoForge version 21.0.31+.

--- NeoForge/src/main/java/com/natamus/altereddamage/neoforge/events/NeoForgeAlterDamageEvent.java
+++ NeoForge/src/main/java/com/natamus/altereddamage/neoforge/events/NeoForgeAlterDamageEvent.java
@@ -2,14 +2,14 @@ package com.natamus.altereddamage.neoforge.events;
 
 import com.natamus.altereddamage.events.AlterDamageEvent;
 import net.minecraft.world.entity.LivingEntity;
-import net.neoforged.neoforge.event.entity.living.LivingHurtEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
 @EventBusSubscriber
 public class NeoForgeAlterDamageEvent {
 	@SubscribeEvent
-	public static void onEntityDamageTaken(LivingHurtEvent e) {
+	public static void onEntityDamageTaken(LivingIncomingDamageEvent e) {
 		LivingEntity livingEntity = e.getEntity();
 
 		float originalDamage = e.getAmount();
