588699fa057b9bb08ac731e0f76e362934be8024
Rick South <rick@natamus.com>
2025-02-12 23:46:37 +0100

    (v3.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/altereddamage/forge/events/ForgeAlterDamageEvent.java
+++ Forge/src/main/java/com/natamus/altereddamage/forge/events/ForgeAlterDamageEvent.java
@@ -4,12 +4,10 @@ import com.natamus.altereddamage.events.AlterDamageEvent;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeAlterDamageEvent {
 	@SubscribeEvent
-	public void onEntityDamageTaken(LivingHurtEvent e) {
+	public static void onEntityDamageTaken(LivingHurtEvent e) {
 		LivingEntity livingEntity = e.getEntity();
 
 		float originalDamage = e.getAmount();
