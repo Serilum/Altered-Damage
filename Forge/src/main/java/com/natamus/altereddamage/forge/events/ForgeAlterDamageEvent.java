06f63849bdbefea5616cafc0a9b81c8b7fa121a8
Rick South <rick@natamus.com>
2025-03-28 01:03:16 +0100

    Altered Damage source code for 1.21.5.

---
+++ Forge/src/main/java/com/natamus/altereddamage/forge/events/ForgeAlterDamageEvent.java
@@ -0,0 +1,20 @@
+package com.natamus.altereddamage.forge.events;
+
+import com.natamus.altereddamage.events.AlterDamageEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeAlterDamageEvent {
+	@SubscribeEvent
+	public static void onEntityDamageTaken(LivingHurtEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+
+		float originalDamage = e.getAmount();
+		float newDamage = AlterDamageEvent.onEntityDamageTaken(livingEntity.level(), livingEntity, e.getSource(), originalDamage);
+
+		if (originalDamage != newDamage) {
+			e.setAmount(newDamage);
+		}
+	}
+}
