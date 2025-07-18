8f9dff5ab0fde04b36f77dcb7cb8ea7fe6c7d233
Rick South <rick@natamus.com>
2025-07-18 03:15:01 +0200

    Altered Damage source code for 1.21.8.

---
+++ Common/src/main/java/com/natamus/altereddamage/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.altereddamage;
+
+import com.natamus.altereddamage.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
