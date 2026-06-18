76f5e28ee133c573388536fd0fcfdb3fa3b7762c
Rick South <rick@natamus.com>
2026-06-19 00:15:54 +0200

    Updated Altered Damage to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/altereddamage/util/Reference.java
+++ Common/src/main/java/com/natamus/altereddamage/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "altereddamage";
 	public static final String NAME = "Altered Damage";
 	public static final String VERSION = "3.7";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
